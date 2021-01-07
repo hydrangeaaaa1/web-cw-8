659ea527445153109d592f19435fdfffb8d3d7c8
haya <hydrangeaaaa1@users.noreply.github.com>
2021-01-07 20:42:19 +0300

    done

--- myscript.js
+++ myscript.js
@@ -3,12 +3,63 @@ let board = [
     ['10', '11', '12'],
     ['20', '21', '22']
 ]
+// const winningConditions = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6]
+// ];
+let turn = "O";
 
-
-
-function press(cellNO) {
-
+function press(x,y) {
+if (turn == "O")
+{
+    turn = "X";
+}else{
+    turn = "O";
+}
     // Do stuff
-    console.log(cellNO)
+    board[x][y] = turn;
+    let cell = document.getElementById(`${x}${y}`);
+    cell.innerText = turn;
+    if (board[0][0] == turn && board[0][1] == turn && board[0][2] == turn )
+    {
+        document.getElementById("status").innerText = `${turn} wins`;
+    }
+    if (board[0][0] == turn && board[1][0] == turn && board[2][0] == turn )
+    {
+        document.getElementById("status").innerText = `${turn} wins`;
+    }
+    if (board[0][0] == turn && board[1][1] == turn && board[2][2] == turn )
+    {
+        document.getElementById("status").innerText = `${turn} wins`;
+    }
+
+    if (board[0][1] == turn && board[1][1] == turn && board[2][1] == turn )
+    {
+        document.getElementById("status").innerText = `${turn} wins`;
+    }
+    if (board[0][2] == turn && board[1][2] == turn && board[2][2] == turn )
+    {
+        document.getElementById("status").innerText = `${turn} wins`;
+    }
+    if (board[1][0] == turn && board[2][1] == turn && board[2][2] == turn )
+    {
+        document.getElementById("status").innerText = `${turn} wins`;
+    }
+    if (board[2][0] == turn && board[2][1] == turn && board[2][2] == turn )
+    {
+        document.getElementById("status").innerText = `${turn} wins`;
+    }
+    if (board[0][2] == turn && board[1][1] == turn && board[2][0] == turn )
+    {
+        document.getElementById("status").innerText = `${turn} wins`;
+    }
+    console.log(x,y)
     
 }
+
